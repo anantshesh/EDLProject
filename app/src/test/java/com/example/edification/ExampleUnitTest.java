d15ee9711af81a193f7701cf0026e7dd55878288
Anantshesh Katti <meghacode1706@gmail.com>
2021-07-08 00:48:02 +0530

    Junit dummy code

--- app/src/test/java/com/example/edification/ExampleUnitTest.java
+++ app/src/test/java/com/example/edification/ExampleUnitTest.java
@@ -12,6 +12,8 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
+
+        //Trial Junit code
         assertEquals(4, 2 + 2);
     }
 }
